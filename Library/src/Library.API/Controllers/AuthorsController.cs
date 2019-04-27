7c1c60bb57f3601bf664050c59aa1d0a65f4a39e
LRizoJr <luis_rizo@ultimatesoftware.com>
2019-04-27 18:19:01 -0400

    Initial project creation (project already in progress)

---
+++ Library/src/Library.API/Controllers/AuthorsController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Library.API.Services;
+using Library.API.Models;
+using Library.API.Helpers;
+namespace Library.API.Controllers
+{    
+    [Route("api/authors")]
+    public class AuthorsController : Controller
+    {
+        private ILibraryRepository _libraryRepository;
+
+        public AuthorsController(ILibraryRepository libraryRepository)
+        {
+            _libraryRepository = libraryRepository;
+        }
+
+        [HttpGet()]
+        public IActionResult GetAuthors()
+        {
+            var authorsFromRepo = _libraryRepository.GetAuthors();
+
+            var authors = new List<AuthorDto>();
+            foreach(var author in authorsFromRepo)
+            {
+                authors.Add(new AuthorDto()
+                {
+                    Id = author.Id,
+                    Name = $"{author.FirstName} {author.LastName}",
+                    Genre = author.Genre,
+                    Age = author.DateOfBirth.GetCurrentAge()
+                });
+
+            }
+
+            return new JsonResult(authors);
+        }
+    }
+}
